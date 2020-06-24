d6ee537fd4b3e79aec400115e1ba9f4b66c78358
premchand Peddakotla <lapeddak@microsoft.com>
2020-06-23 22:23:43 -0700

    firstcommit

---
+++ src/build/precache-manifest.c1b5ba8712f4212e488103bc05ad82b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c180ad33d155ac8ff4a23a4439bf165d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b9c79a03854b2c3f640",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "523ee6f5b9c934add56a",
+    "url": "/static/js/2.fea61d2a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fea61d2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b9c79a03854b2c3f640",
+    "url": "/static/js/main.bb51f2bd.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
